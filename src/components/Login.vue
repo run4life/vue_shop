77ec7278e2fbdaf3d8ae239ae4c1c56637c448b3
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-03-22 18:04:29 +0800

    add login

---
+++ src/components/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <div class="sysname">电商后台管理系统</div>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type=password></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        },
+        loginFormRules: {
+          username: [
+            { required: true, message: '请输入登录名称', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入登录密码', trigger: 'blur' },
+            { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      resetLoginForm() {
+        this.$refs.loginFormRef.resetFields()
+      },
+      login() {
+        this.$refs.loginFormRef.validate(async valid => {
+          if (!valid) return false
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) return this.$message.error('登录失败')
+          this.$message.success('登录成功')
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('/home')
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%)
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+
+  .sysname {
+    font-size: 35px;
+    font-weight: bold;
+    color: #2b4b6b;
+    position: absolute;
+    top: -80px;
+    left: 80px;
+  }
+</style>
